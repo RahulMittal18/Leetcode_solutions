07ab6fefbff999df6184a96de71501c56f45ca2d
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-05-01 15:09:53 +0530

    Time: 58 ms (23.62%), Space: 25.8 MB (82.33%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -1,46 +1,18 @@
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& nums) {
-        vector<int> ans;
-        int l=0,r;
-        
-        if(nums[nums.size()-1]<0){
-            r=nums.size();
-            l=nums.size()-1;
-        }
-        else if(nums[0]>=0){
-            r=0;
-            l=-1;
-        }
-        else{
-            while(l<nums.size()-1){
-            if(nums[l+1]>=0 && nums[l]<0)
-                break;
-            l++;
-            }
-            r=l+1;
-        }
-            
-        while(l>=0 || r<nums.size()){
-            if(l>=0 and r< nums.size()){
-                if(-nums[l]<nums[r]){
-                    ans.push_back(nums[l]*nums[l]);
-                    l-=1;
-                }
-                else{
-                    ans.push_back(nums[r]*nums[r]);
-                    r+=1;
-                }
-            }
-            else if(l>=0){
-                ans.push_back(nums[l]*nums[l]);
-                l-=1;
-            }
-            else{
-                ans.push_back(nums[r]*nums[r]);
-                    r+=1;
+    vector<int> sortedSquares(vector<int>& A) {
+        int n = A.size();
+        vector<int> result(n);
+        int i = 0, j = n - 1;
+        for (int p = n - 1; p >= 0; p--) {
+            if (abs(A[i]) > abs(A[j])) {
+                result[p] = A[i] * A[i];
+                i++;
+            } else {
+                result[p] = A[j] * A[j];
+                j--;
             }
         }
-        return ans;
+        return result;
     }
 };
