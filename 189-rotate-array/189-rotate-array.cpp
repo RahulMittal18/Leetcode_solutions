a7471cd09beb3040626dd1b4a95316c8e7af997b
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-05-01 16:09:33 +0530

    Time: 42 ms (43.88%), Space: 25 MB (81.36%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int size= nums.size();
+        vector<int> ans;
+        k=k%size;
+        if(k!=0){
+            reverse(nums.begin(),nums.end());
+            reverse(nums.begin(), nums.begin()+k);
+            reverse(nums.begin()+k, nums.end());
+        }
+    }
+};
