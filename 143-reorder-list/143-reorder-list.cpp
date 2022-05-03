ad8287a2ae60a1011a24ff902eeac084f9e59d36
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-05-03 10:57:58 +0530

    Time: 49 ms (62.91%), Space: 17.7 MB (78.09%) - LeetHub

---
+++ 143-reorder-list/143-reorder-list.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if(!head->next) return;
+        
+        // Finding middle of linked list
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast->next and fast->next->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        
+        // Reversing the second half of the linked list
+        ListNode* prev=NULL;
+        ListNode* curr = slow->next;
+        while(curr){
+            ListNode* nextt = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr=nextt;
+        }
+        slow->next = NULL;
+        
+        // Merge two lists 
+        ListNode* head1=head;
+        ListNode* head2=prev;
+        while(head2){
+            ListNode* temp = head1->next;
+            head1->next = head2;
+            head1=head1->next;
+            head2= temp;
+        }
+        
+    }
+};
