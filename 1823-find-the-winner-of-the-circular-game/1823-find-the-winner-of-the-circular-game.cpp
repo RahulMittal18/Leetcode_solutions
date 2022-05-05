c9a5b8a0d79e43ebf71abba89f551f16c65ccb86
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-05-05 17:00:47 +0530

    Time: 3 ms (73.51%), Space: 6 MB (73.47%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int jos(int n, int k){
+        if(n==1)
+            return 0;
+        else
+            return (jos(n-1,k)+k)%n;
+    }
+    int findTheWinner(int n, int k) {
+        return jos(n,k)+1;
+    }
+};
