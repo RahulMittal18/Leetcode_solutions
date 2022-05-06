0cc513be57abb392d8c1e2a5e57f02e69d04f78b
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-05-07 01:10:35 +0530

    Time: 3 ms (93.63%), Space: 12.8 MB (67.63%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -1,47 +1,54 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
+    void markParents(TreeNode* root, unordered_map<TreeNode*, TreeNode*> &parent_track, TreeNode* target) {
+        queue<TreeNode*> queue;
+        queue.push(root);
+        while(!queue.empty()) { 
+            TreeNode* current = queue.front(); 
+            queue.pop();
+            if(current->left) {
+                parent_track[current->left] = current;
+                queue.push(current->left);
+            }
+            if(current->right) {
+                parent_track[current->right] = current;
+                queue.push(current->right);
+            }
+        }
+    }
 public:
-    set<TreeNode*> vis;
-    void mapParents(TreeNode* root, map<TreeNode*,TreeNode*>& parent){
-        if(!root)
-            return;
-        if(root->left){
-            parent[root->left]=root;
-            mapParents(root->left,parent);
-        }
-        if(root->right){
-            parent[root->right]=root;
-            mapParents(root->right,parent);
-        }
-    }
-    
-    void solve(TreeNode* root, int k,map<TreeNode*,TreeNode*>& parent, vector<int>& ans){
-        if(vis.count(root)) return;
-        if(!root)
-            return;
-        vis.insert(root);
-        if(k==0) {ans.push_back(root->val); return;}
-        solve(parent[root],k-1,parent,ans);
-        solve(root->left,k-1,parent,ans);
-        solve(root->right,k-1,parent,ans);
-    }
-    
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        map<TreeNode*,TreeNode*> parent;
-        parent[root]=nullptr;
-        mapParents(root,parent);
+        unordered_map<TreeNode*, TreeNode*> parent_track; // node -> parent
+        markParents(root, parent_track, target); 
         
-        
-        vector<int> ans;
-        solve(target,k,parent,ans);
-        return ans;
+        unordered_map<TreeNode*, bool> visited; 
+        queue<TreeNode*> queue;
+        queue.push(target);
+        visited[target] = true;
+        int curr_level = 0;
+        while(!queue.empty()) { /*Second BFS to go upto K level from target node and using our hashtable info*/
+            int size = queue.size();
+            if(curr_level++ == k) break;
+            for(int i=0; i<size; i++) {
+                TreeNode* current = queue.front(); queue.pop();
+                if(current->left && !visited[current->left]) {
+                    queue.push(current->left);
+                    visited[current->left] = true;
+                }
+                if(current->right && !visited[current->right]) {
+                    queue.push(current->right);
+                    visited[current->right] = true;
+                }
+                if(parent_track[current] && !visited[parent_track[current]]) {
+                    queue.push(parent_track[current]);
+                    visited[parent_track[current]] = true;
+                }
+            }
+        }
+        vector<int> result;
+        while(!queue.empty()) {
+            TreeNode* current = queue.front(); queue.pop();
+            result.push_back(current->val);
+        }
+        return result;
     }
 };
