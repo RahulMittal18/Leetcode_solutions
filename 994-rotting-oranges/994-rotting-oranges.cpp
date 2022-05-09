4ec96d04e3452af775196c29dc48db51722e11ea
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-05-09 13:08:22 +0530

    Time: 9 ms (57.48%), Space: 13 MB (79.82%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -9,37 +9,32 @@ public:
         queue<pair<int,int>> q;
         int fresh=0;
         for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++)
-            {
-                if(grid[i][j]==2)
-                    q.push({i,j});
-                if(grid[i][j]==1)
-                    fresh++;
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2) q.push({i,j});
+                if(grid[i][j]==1) fresh++;
             }
         int ans=-1;
         while(!q.empty())
         {
             int sz=q.size();
-            while(sz--)
-            {
+            while(sz--){
                 pair<int,int> p=q.front();
                 q.pop();
-                for(int i=0;i<4;i++)
-                {
+            
+                for(int i=0;i<4;i++){
                     int r=p.first+dir[i];
                     int c=p.second+dir[i+1];
-                    if(r>=0 && r<m && c>=0 && c<n &&grid[r][c]==1)
-                    {
-                        grid[r][c]=2;
-                        q.push({r,c});
-                        fresh--; 
-                    }
+                    if(r<0 || r>=m || c<0 || c>=n || grid[r][c]!=1) continue;
                     
+                    grid[r][c]=2;
+                    q.push({r,c});
+                    fresh--;  
                 }
             }
+        
             ans++; 
         }
-        if(fresh>0) return -1; //if fresh>0 that means there are fresh oranges left
+        if(fresh>0) return -1;
         if(ans==-1) return 0; //we initialised with -1, so if there were no oranges it'd take 0 mins.
         return ans;
         
