389d06e3ffb9b56d4810639a60fb7b93255540d4
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-05-11 19:26:23 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (16.16%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    Solution(){
+        
+        // Initialization
+        // Base case:
+        climbMethodTo[0] = 1;   // reach floor_0 by doing nothing
+        climbMethodTo[1] = 1;   // reach floor_1 with one step
+    }
+    
+    int climbStairs(int n) {
+        
+        // look-up table, also known as memoization
+        if( climbMethodTo[n] ){
+            return climbMethodTo[n];
+        }
+        
+
+        climbMethodTo[n] = climbStairs(n-1) + climbStairs(n-2);
+        
+        return climbMethodTo[n];
+    }
+    
+private:
+    map<int, int> climbMethodTo;
+};
