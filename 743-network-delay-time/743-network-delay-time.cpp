60a2bc307c3d2dd0d3013837607740ec0191adb6
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-05-14 21:48:15 +0530

    Time: 228 ms (23.33%), Space: 43.3 MB (45.63%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
-    void dfs(int src, vector<pair<int,int>> edges[],vector<int>& vis,int temp,int& ans){
-        if(temp>=vis[src]) return;
-        vis[src]=temp;
-        for(auto adj:edges[src]){
-                dfs(adj.first,edges,vis,temp+adj.second,ans);
-        }
-    }
-    
+    void dij(int src, vector<pair<int,int>> edges[],vector<int>& vis){
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        pq.push({0, src});
+        vis[src]=0;
+        while(!pq.empty()){
+            auto curr = pq.top();
+            // cout<<curr.first<<" "<<curr.second<<" "<< vis[curr.second]<<endl;
+            pq.pop();
+            if(curr.first > vis[curr.second]) continue;
+            for(auto adj:edges[curr.second]){
+                // cout<<adj.first<<endl;
+                // cout<<adj.first<<" "<<vis[adj.first] <<" "<< curr.first <<" "<< adj.second<<pq.size()<<endl;
+                if(vis[adj.first]> curr.first + adj.second){
+                    vis[adj.first] = curr.first + adj.second;
+                                
+
+                    pq.push({vis[adj.first],adj.first});
+                }
+            }    
+        }        
+    } 
     
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<pair<int,int>> edges[n];
         for(auto x:times){
             edges[x[0]-1].push_back({x[1]-1,x[2]});
+            // cout<<x[0]<<" "<<x[1]<<endl;
         }
-         for (int i = 0; i < n; i++) {
-            sort(edges[i].begin(), edges[i].end());
-        }
+        // for(auto x:edges) {
+        //     for(auto y:x) cout<<y.first<<" "<<y.second<<endl;
+        // }
+        
         vector<int> vis(n,1e8);
-        int ans=INT_MIN;
-        int temp=0;
-        dfs(k-1,edges,vis,temp,ans);
+        int ans=0;
+        dij(k-1,edges,vis);
         for(auto x: vis){
+            // cout<<x<<endl;
             ans=max(ans,x);
         }
         return ans==1e8 ? -1 : ans;
