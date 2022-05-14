7cf4e4255c056919b07668f021946b6048cc5088
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-05-14 21:48:44 +0530

    Time: 146 ms (65.89%), Space: 43.6 MB (35.11%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -6,16 +6,11 @@ public:
         vis[src]=0;
         while(!pq.empty()){
             auto curr = pq.top();
-            // cout<<curr.first<<" "<<curr.second<<" "<< vis[curr.second]<<endl;
             pq.pop();
             if(curr.first > vis[curr.second]) continue;
             for(auto adj:edges[curr.second]){
-                // cout<<adj.first<<endl;
-                // cout<<adj.first<<" "<<vis[adj.first] <<" "<< curr.first <<" "<< adj.second<<pq.size()<<endl;
                 if(vis[adj.first]> curr.first + adj.second){
                     vis[adj.first] = curr.first + adj.second;
-                                
-
                     pq.push({vis[adj.first],adj.first});
                 }
             }    
@@ -26,17 +21,12 @@ public:
         vector<pair<int,int>> edges[n];
         for(auto x:times){
             edges[x[0]-1].push_back({x[1]-1,x[2]});
-            // cout<<x[0]<<" "<<x[1]<<endl;
         }
-        // for(auto x:edges) {
-        //     for(auto y:x) cout<<y.first<<" "<<y.second<<endl;
-        // }
         
         vector<int> vis(n,1e8);
         int ans=0;
         dij(k-1,edges,vis);
         for(auto x: vis){
-            // cout<<x<<endl;
             ans=max(ans,x);
         }
         return ans==1e8 ? -1 : ans;
