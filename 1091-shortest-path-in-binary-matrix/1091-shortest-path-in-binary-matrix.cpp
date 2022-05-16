271d2260a2688808b1c60d0a7dfd128d3595178a
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-05-16 13:45:49 +0530

    Time: 441 ms (11.61%), Space: 74.9 MB (14.24%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -8,12 +8,12 @@ public:
         if (col == 0 ) return -1;
         if (grid[0][0] != 0 | grid[row-1][col-1] != 0) return -1;
 
-        queue<pair<int, int>> queue;
-        queue.push({0,0});
+        queue<pair<int, int>> q;
+        q.push({0,0});
     vector<vector<int>>directions={{1,1},{0,1},{1,0},{0,-1},{-1,0},{-1,-1},{1,-1},{-1, 1}};
             grid[0][0] = 1;
-            while(!queue.empty()){
-                auto curr = queue.front();
+            while(!q.empty()){
+                auto curr = q.front();
                 int x = curr.first, y = curr.second;
                 if( x == row -1 && y == col -1) return grid[x][y];
 
@@ -21,11 +21,11 @@ public:
                     int nx = x + direction[0];
                     int ny = y + direction[1];
                     if(nx >= 0 && nx < row && ny >= 0 && ny < col && grid[nx][ny] == 0){
-                        queue.push({nx,ny});
+                        q.push({nx,ny});
                         grid[nx][ny] = grid[x][y] + 1;
                     }
                 }
-                queue.pop();
+                q.pop();
             }
             return -1;
     }
