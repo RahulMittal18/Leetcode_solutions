c860aedfe5295949c20f2f4954a35c1081ee7fcd
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-05-22 08:45:49 +0530

    Time: 34 ms (30.35%), Space: 10.7 MB (35.24%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int ans=0;
-        for(int i=0; i<s.length(); i++){
-            // for odd length palindromes
-            int l=i,r=i;
-            while(l>=0 and r<s.length() and s[l]==s[r]){
-                ans++;
-                l--;
-                r++;
-            }
-            
-            // for even length palindromes
-            l=i;
-            r=i+1;
-            while(l>=0 and r<s.length() and s[l]==s[r]){
-                ans++;
-                l--;
-                r++;
-            }
+        int n = s.size();
+        
+        int dp[n+1][n+1];
+        memset(dp, 0, sizeof(dp));
+        
+        int ans = 0;
+        
+		// dp[i][j] == 1 for every i==j as every char is a pallindromic substring of length 1
+        for(int i=0; i<n; i++){
+            dp[i][i] = 1;
+            ans++;
         }
+            
+			
+        for(long long i=2; i<=n; i++){
+            for(int j=0; j+i<=n; j++){
+                int k = j+i-1;
+                
+                if(i==2 && s[j] == s[k]) {
+                    dp[j][k] = 1;
+                    ans++;
+                }
+                else if(s[j] == s[k] && dp[j+1][k-1]!=0){ 
+                    dp[j][k] = 1;
+                    ans++;
+                }
+            }
+        }        
         return ans;
     }
 };
