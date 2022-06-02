9d5c6dbb384ed201d038c8534284bc7a21fd6d9b
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-06-02 21:42:53 +0530

    Time: 31 ms (94.25%), Space: 13.4 MB (31.61%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(begin(nums), end(nums));
+        sort(nums.begin(), nums.end());
         return kSum(nums, target, 0, 4);
     }
 	
@@ -18,13 +18,13 @@ public:
         // We cannot obtain a sum of target if the smallest value
         // in nums is greater than target / k or if the largest 
         // value in nums is smaller than target / k.
-        if  (nums[start] > average_value || average_value > nums.back()) return res;
+        if (nums[start] > average_value || average_value > nums.back()) return res;
             
         if (k == 2) return twoSum(nums, target, start);
     
         for (int i = start; i < nums.size(); ++i) {
             if (i == start || nums[i - 1] != nums[i]) {
-                for (vector<int>& subset : kSum(nums, target-nums[i], i+1, k-1)) {
+                for(vector<int>& subset : kSum(nums, target-nums[i], i+1, k-1)) {
                     res.push_back({nums[i]});
                     res.back().insert(end(res.back()), subset.begin(), subset.end());
                 }
@@ -37,13 +37,15 @@ public:
         vector<vector<int>> res;
         int lo = start, hi = nums.size() - 1;
     
-        while (lo < hi) {
+        while(lo < hi){
             int curr_sum = nums[lo] + nums[hi];
             if (curr_sum < target || (lo > start && nums[lo] == nums[lo - 1])) {
                 ++lo;
-            } else if (curr_sum > target || (hi < nums.size() - 1 && nums[hi] == nums[hi + 1])) {
+            }
+            else if (curr_sum > target || (hi < nums.size() - 1 && nums[hi] == nums[hi + 1])) {
                 --hi;
-            } else {
+            }
+            else {
                 res.push_back({ nums[lo++], nums[hi--] });
             }
         }                                                   
