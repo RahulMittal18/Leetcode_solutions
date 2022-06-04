007c0ee0342651ada2117407a7d02668fe29b278
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-06-04 20:21:06 +0530

    Time: 51 ms (21.89%), Space: 45.5 MB (15.74%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,41 +1,32 @@
 class Solution {
-public:
-    
-    bool isSafe(int row,int col,vector<string> temp,int n){
-        for(int i=row,j=col;i>=0 && j>=0;i--,j--){
-            if(temp[i][j]=='Q') return false;
-        }
-        
-        for(int i=row,j=col;i<n && j>=0;i++,j--){
-            if(temp[i][j]=='Q') return false;
-        }
-        
-        for(int i=row,j=col;j>=0;j--){
-            if(temp[i][j]=='Q') return false;
-        }
-        
+public:  
+    bool isSafe(int row,int col,vector<string> temp,int n,vector<bool> &rowQ,vector<bool> &upperDiag,vector<bool> &lowerDiag){
+            if(rowQ[row] or upperDiag[row-col+n-1] or lowerDiag[row+col]) return false;        
         return true;
     }
     
     
-    void solve(int col,vector<string> &temp,vector<vector<string>> &ans, int n){
+    void solve(int col,vector<string> &temp,vector<vector<string>> &ans, int n,vector<bool> &rowQ,vector<bool> &upperDiag,vector<bool> &lowerDiag){
         if(col==n){
             ans.push_back(temp);
             return;
         }
         
         for(int row=0;row<n;row++){
-            if(isSafe(row,col,temp,n)){
+            if(isSafe(row,col,temp,n,rowQ,upperDiag,lowerDiag)){
                 temp[row][col]='Q';
-                solve(col+1,temp,ans,n);
+                rowQ[row]=true;
+                upperDiag[row-col+n-1]=true;
+                lowerDiag[row+col]=true;
+                solve(col+1,temp,ans,n,rowQ,upperDiag,lowerDiag);
                 temp[row][col]='.';
+                rowQ[row]=false;
+                upperDiag[row-col+n-1]=false;
+                lowerDiag[row+col]=false;
             }
         }
         
-    }
-    
-    
-    
+    }    
     
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
@@ -44,7 +35,10 @@ public:
         for(int i=0;i<n;i++){
             vec[i]=s;
         }
-        solve(0,vec,ans,n);
+        vector<bool> rowQ(n,false);
+        vector<bool> upperDiag(2*n-1,false);
+        vector<bool> lowerDiag(2*n-1,false);
+        solve(0,vec,ans,n,rowQ,upperDiag,lowerDiag);
         return ans;
     }
 };
