c94bab35ceb8527c63121cd7969f1df7efcbdf99
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-06-13 13:43:52 +0530

    Time: 89 ms (50.26%), Space: 40 MB (31.77%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
-    
-    int kadane1(vector<int>& arr){
-        int ans = INT_MIN;
-        int sum=0;
-        for(int x:arr){
-            sum = max(x,x+sum);
-            ans = max(ans,sum);
-        }
-        return ans;
-    }
-    
-    
-    int kadane2(vector<int>& arr){
-        int ans = INT_MAX;
-        int sum=0;
-        for(int x:arr){
-            sum = min(x,x+sum);
-            ans = min(ans,sum);
-        }
-        return ans;
-    }
-    
+   
     int maxSubarraySumCircular(vector<int>& nums) {
-        int a1 = kadane1(nums);
-        int a2 = kadane2(nums);
-        int a3 = accumulate(nums.begin(),nums.end(),0ll);
-        return a3-a2!=0?max(a1,a3-a2):a1;
+        int maxans = INT_MIN;
+        int minans = INT_MAX;
+        int minsum = 0;
+        int maxsum = 0;
+        int totalsum = 0;
+        for(int x:nums){
+            totalsum+=x;
+            maxsum = max(x,x+maxsum);
+            maxans = max(maxans,maxsum);
+            minsum = min(minsum+x,x);
+            minans = min(minans,minsum);
+        }
+        
+        return totalsum==minans?maxans:max(totalsum-minans,maxans);
     }
 };
