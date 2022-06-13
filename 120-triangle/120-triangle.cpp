ed5e6b02d7b150746ff4866ce0fa5aa116083214
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-06-13 13:16:35 +0530

    Time: 102 ms (42.59%), Space: 17.1 MB (7.66%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,22 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        def recurse(r,c):
+
+            # base
+            if r == len(grid)-1:
+                return grid[r][c]
+
+
+            # memo cond
+            if (r,c) in memo:
+                return memo[(r,c)]
+
+            # recursive - O(1) is the cost of the recursive call.- isolated
+            result = grid[r][c] + min(recurse(r+1, c), recurse(r+1, c+1))
+            memo[(r,c)] = result
+            return result
+
+        # main 
+        memo = {}
+        grid = triangle
+        return recurse(0,0)
