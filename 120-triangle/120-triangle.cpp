834bd00d75488deda261fd41a65bc84d2d42dd56
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-06-13 13:24:29 +0530

    Time: 16 ms (11.39%), Space: 8.8 MB (55.37%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int n,dp[202][202];
     int solve(vector<vector<int>> &t,int i,int j){
-        if(i==n) return 0;
+        if(i==n-1) return t[i][j];
         if(dp[i][j]!=-1) return dp[i][j];
         return dp[i][j]= t[i][j]+ min(solve(t,i+1,j),solve(t,i+1,j+1));
     }
