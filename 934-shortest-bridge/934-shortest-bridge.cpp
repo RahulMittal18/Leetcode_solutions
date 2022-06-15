78007008861167743d6157f28a4703cabf855303
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-06-15 17:19:47 +0530

    Time: 57 ms (77.74%), Space: 18.8 MB (85.66%) - LeetHub

--- 934-shortest-bridge/934-shortest-bridge.cpp
+++ 934-shortest-bridge/934-shortest-bridge.cpp
@@ -4,9 +4,7 @@ public:
     void dfs(vector<vector<int>>& grid,int i,int j,queue<pair<int,int>> &q){
         grid[i][j]=-1;
         q.push({i,j});
-
         int dir[5]={0,1,0,-1,0};
-
         for(int k=0;k<4;k++){
             int x = i + dir[k];
             int y = j + dir[k+1];
@@ -32,33 +30,30 @@ public:
                 }
             }
         }
+        
         int i,j;
-                int dir[5]={0,1,0,-1,0};
+        int dir[5]={0,1,0,-1,0};
         int d =0;
-        cout<<q.size();;
         while(!q.empty()){
 
             int nn = q.size();
             while(nn--){
-            tie(i,j) = q.front();
-            q.pop();
-            for(int k=0;k<4;k++){
-                int x = i+dir[k];
-                int y = j+dir[k+1];
-                if(x>=0 and y>=0 and x<m and y<n){
-                    if(grid[x][y]==1)
-                        return d;
-                    else if(grid[x][y]==0){
-                        q.push({x,y});
-                        grid[x][y]=-1;
-
+                tie(i,j) = q.front();
+                q.pop();
+                for(int k=0;k<4;k++){
+                    int x = i+dir[k];
+                    int y = j+dir[k+1];
+                    if(x>=0 and y>=0 and x<m and y<n){
+                        if(grid[x][y]==1)
+                            return d;
+                        else if(grid[x][y]==0){
+                            q.push({x,y});
+                            grid[x][y]=-1;
+                        }
                     }
                 }
             }
-
-            }
-                                        d++;
-
+            d++;
         }
         return 0;
     }
