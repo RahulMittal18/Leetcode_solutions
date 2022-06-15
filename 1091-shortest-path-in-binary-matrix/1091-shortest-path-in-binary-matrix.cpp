cb45d07a4931a062d6cd5ee3ea84d9d66aa53be6
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-06-15 12:10:42 +0530

    Time: 643 ms (5.01%), Space: 74.9 MB (8.43%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -10,23 +10,23 @@ public:
 
         queue<pair<int, int>> q;
         q.push({0,0});
-    vector<vector<int>>directions={{1,1},{0,1},{1,0},{0,-1},{-1,0},{-1,-1},{1,-1},{-1, 1}};
-            grid[0][0] = 1;
-            while(!q.empty()){
-                auto curr = q.front();
-                int x = curr.first, y = curr.second;
-                if( x == row -1 && y == col -1) return grid[x][y];
+        vector<vector<int>>directions={{1,1},{0,1},{1,0},{0,-1},{-1,0},{-1,-1},{1,-1},{-1, 1}};
+        grid[0][0] = 1;
+        while(!q.empty()){
+            auto curr = q.front();
+            int x = curr.first, y = curr.second;
+            if( x == row -1 && y == col -1) return grid[x][y];
 
-                for(auto direction : directions){
-                    int nx = x + direction[0];
-                    int ny = y + direction[1];
-                    if(nx >= 0 && nx < row && ny >= 0 && ny < col && grid[nx][ny] == 0){
-                        q.push({nx,ny});
-                        grid[nx][ny] = grid[x][y] + 1;
-                    }
+            for(auto direction : directions){
+                int nx = x + direction[0];
+                int ny = y + direction[1];
+                if(nx >= 0 && nx < row && ny >= 0 && ny < col && grid[nx][ny] == 0){
+                    q.push({nx,ny});
+                    grid[nx][ny] = grid[x][y] + 1;
                 }
-                q.pop();
             }
-            return -1;
+            q.pop();
+        }
+        return -1;
     }
 };
