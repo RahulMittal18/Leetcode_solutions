8b2d9932ee53e4c0d65aecc4a0a62bccf614d770
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-06-15 12:14:34 +0530

    Time: 62 ms (95.13%), Space: 19.3 MB (83.81%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int res = 1;
-        int row = grid.size();
-        if (row == 0) return -1;
-        int col = grid[0].size();
-        if (col == 0 ) return -1;
-        if (grid[0][0] != 0 | grid[row-1][col-1] != 0) return -1;
-
+        if (grid[0][0] != 0) return -1;
+        int N = grid.size();
         queue<pair<int, int>> q;
-        q.push({0,0});
-        vector<int> directions[8]={{1,1},{0,1},{1,0},{0,-1},{-1,0},{-1,-1},{1,-1},{-1, 1}};
+        q.push({0, 0});
+        int count = 0;
         grid[0][0] = 1;
-        while(!q.empty()){
-            pair<int,int> curr = q.front();
-            int x = curr.first, y = curr.second;
-            if( x == row-1 && y == col-1) return grid[x][y];
+        pair<int, int> curr;
+        int i, j;
+        int dirs_i[8] = {-1, 0, 1, 1, 1, 0, -1, -1};
+        int dirs_j[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
+        while (!q.empty()) {
+            curr = q.front();
+            q.pop();
+            i = curr.first;
+            j = curr.second;
 
-            for(auto direction : directions){
-                int nx = x + direction[0];
-                int ny = y + direction[1];
-                if(nx >= 0 && nx < row && ny >= 0 && ny < col && grid[nx][ny] == 0){
-                    q.push({nx,ny});
-                    grid[nx][ny] = grid[x][y] + 1;
+            count = grid[i][j];
+
+            if (i == N - 1 && j == N - 1)
+                return grid[i][j];
+            for (int k = 0; k < 8; k++) {
+                int n_i = i + dirs_i[k];
+                int n_j = j + dirs_j[k];
+                if (n_i < 0 || n_i >= N) continue;
+                if (n_j < 0 || n_j >= N) continue;
+                if (grid[n_i][n_j] == 0) {
+                    grid[n_i][n_j] = count + 1;
+                    q.push({n_i, n_j});
                 }
             }
-            q.pop();
+            
         }
         return -1;
     }
