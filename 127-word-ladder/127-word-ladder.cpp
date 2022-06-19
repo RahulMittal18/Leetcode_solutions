eeab4a0eca5c668c017117c4e85b013199f6b8f0
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-06-19 21:19:40 +0530

    Time: 1237 ms (18.95%), Space: 15.9 MB (41.33%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        set<string> s;
+        for(auto x:wordList) s.insert(x);
+        set<string> vis;
+        vis.insert(beginWord);
+        queue<string> q;
+        q.push(beginWord);
+        int ans=1;
+        while(!q.empty()){
+            
+            int n = q.size();
+
+            while(n--){
+                string popped = q.front();
+                q.pop();
+                if (popped==endWord) return ans;
+            // cout<<n<<endl;
+                for(int i=0;i<popped.size();i++){
+                    for(int j=0;j<26;j++){
+                    string temp = popped;
+                    temp[i] = 97 + (int(temp[i]-'a'+j))%26;
+                    // cout<<popped[i]<<" "<<i<< " ";
+                    // cout<<temp<<endl;
+                    if(vis.count(temp)==0 and s.count(temp)!=0){
+                        // cout<<temp<<ans<<endl;
+                        q.push(temp);
+                        vis.insert(temp);
+                    }
+                }
+                }
+            }
+            ans++;
+        }
+        return 0;
+    }
+};
