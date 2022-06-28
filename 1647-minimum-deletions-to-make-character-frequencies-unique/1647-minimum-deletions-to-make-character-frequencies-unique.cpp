acde5d92df201b7827a318cbc21d86079c7bcc82
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-06-28 11:35:59 +0530

    Time: 115 ms (35.20%), Space: 17.2 MB (95.28%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        int freq[26]={0};
-        for(char c:s) freq[c-'a']++;
-        map<int,set<char>> mp;
-        for(int i=0;i<26;i++) mp[freq[i]].insert(i+'a');
-        int ans = 0;
-        mp[0].clear();
-        for(int i=0;i<26;i++){
-            if(freq[i]!=0){
-                if(mp[freq[i]].size()!=1){
-                    mp[freq[i]].erase(i+'a');
-                    while(mp[freq[i]].size()!=0){
-                        ans++;
-                        freq[i]--;
-                    }
-                    if(freq[i]!=0)
-                    mp[freq[i]].insert(i+'a');
-                }
-            }
+        // Store the frequency of each character
+        vector<int> frequency(26, 0);
+        for (char c : s) {
+            frequency[c - 'a']++;
         }
-        return ans;
+        sort(frequency.begin(), frequency.end(), greater<int>());
+        
+        int deleteCount = 0;
+        // Maximum frequency the current character can have
+        int maxFreqAllowed = s.size();
+        
+        // Iterate over the frequencies in descending order
+        for (int i = 0; i < 26 && frequency[i] > 0; i++) {
+            // Delete characters to make the frequency equal the maximum frequency allowed
+            if (frequency[i] > maxFreqAllowed) {
+                deleteCount += frequency[i] - maxFreqAllowed;
+                frequency[i] = maxFreqAllowed;
+            }
+            // Update the maximum allowed frequency
+            maxFreqAllowed = max(0, frequency[i] - 1);
+        }
+        
+        return deleteCount;
     }
 };
