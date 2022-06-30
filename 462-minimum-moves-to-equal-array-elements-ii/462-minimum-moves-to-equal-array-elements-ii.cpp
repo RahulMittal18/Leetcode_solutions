9f94ac905d2d672ec1a034a05db0aec4cc20566d
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-06-30 13:59:02 +0530

    Time: 27 ms (9.46%), Space: 10.7 MB (99.77%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -4,24 +4,9 @@ public:
         sort(nums.begin(),nums.end());
         int n = nums.size();
         int ans = 0;
-        if(n&1){
-            int med = n/2;
-            for(int x:nums){
-                ans+=abs(nums[med]-x);
-            }
-        }
-        else{
-            int med1 = n/2;
-            int med2 = n/2 -1;
-            int a1 = 0;
-            for(int x:nums){
-                a1+=abs(nums[med1]-x);
-            }
-            int a2 = 0;
-            for(int x:nums){
-                a2+=abs(nums[med2]-x);
-            }
-            ans=min(a1,a2);
+        int med = n/2;
+        for(int x:nums){
+            ans+=abs(nums[med]-x);
         }
         return ans;
     }
