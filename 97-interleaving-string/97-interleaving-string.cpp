248218e9065e5a688ad7223e5c4566c5f3612353
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-07-07 12:01:54 +0530

    Time: 58 ms (21.09%), Space: 33.6 MB (16.02%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    unordered_map<string,bool> mp;
+    int mp[101][101];
     
-    bool helper(string s1,string s2,string s3,int i,int j,int k,int m,int n,int r){
-        if(k==r){
+    bool helper(string s1,string s2,string s3,int i,int j,int m,int n,int r){
+        if(i+j==r){
             return (i==m && j==n);
         }
-        string key = to_string(i) + "-" + to_string(j) + "-" + to_string(k);
-        if(mp.find(key)!=mp.end()) return mp[key];
+        int k = i+j;
+        if(mp[i][j]!=-1) return mp[i][j];
         if(i==m){
-            return mp[key] = s2[j]==s3[k] ? helper(s1,s2,s3,i,j+1,k+1,m,n,r) : false;
+            return mp[i][j] = s2[j]==s3[k] ? helper(s1,s2,s3,i,j+1,m,n,r) : false;
         }
         if(j==n){
-            return mp[key] = s1[i]==s3[k] ? helper(s1,s2,s3,i+1,j,k+1,m,n,r) : false;
+            return mp[i][j] = s1[i]==s3[k] ? helper(s1,s2,s3,i+1,j,m,n,r) : false;
         }
         bool op1 = false, op2 = false;
-        op1 = s2[j]==s3[k] ? helper(s1,s2,s3,i,j+1,k+1,m,n,r) : false;
-        op2 = s1[i]==s3[k] ? helper(s1,s2,s3,i+1,j,k+1,m,n,r) : false;
-        return mp[key]=op1||op2;
+        op1 = s2[j]==s3[k] ? helper(s1,s2,s3,i,j+1,m,n,r) : false;
+        op2 = s1[i]==s3[k] ? helper(s1,s2,s3,i+1,j,m,n,r) : false;
+        return mp[i][j]=op1||op2;
     }
     
     
     bool isInterleave(string s1, string s2, string s3) {
+        memset(mp,-1,sizeof mp);
         int m,n,r;
         m=s1.length();
         n=s2.length();
         r=s3.length();
         if(m+n!=r) return false;
-        return helper(s1,s2,s3,0,0,0,m,n,r);
+        return helper(s1,s2,s3,0,0,m,n,r);
     }
 };
