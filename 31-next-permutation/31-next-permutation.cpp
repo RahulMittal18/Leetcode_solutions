c33cd2f703075e6ae41428906f96f6875808e939
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-07-12 23:59:28 +0530

    Time: 11 ms (24.84%), Space: 12 MB (93.06%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        if(next_permutation(nums.begin(),nums.end())){
-            
-        }
-        else{
-            sort(nums.begin(),nums.end());
-        }
+        next_permutation(nums.begin(),nums.end());
     }
 };
