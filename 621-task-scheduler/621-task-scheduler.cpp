7b49f7fcd44010feca5538b10ff436e232b4c266
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-07-22 19:50:46 +0530

    Time: 114 ms (64.28%), Space: 34.6 MB (44.49%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.cpp
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -3,33 +3,31 @@ public:
     int leastInterval(vector<char>& tasks, int n) {
         priority_queue<pair<int,char>> pq,temp;
         int freq[26]={0};
-        for(auto x:tasks){
-            freq[x-'A']++;
-        }
-        // sort(freq,freq+26,greater<int>());
+        for(auto x:tasks) freq[x-'A']++;
         int time = 1;
+        
         for(int i=0;i<26;i++){
-            if(freq[i]!=0){
-                pq.push({freq[i],'A'+ i });
-            }
+            if(freq[i]!=0) pq.push({freq[i],'A'+ i });
         }
+        
         unordered_map<char,int> mp;
         for(int i=0;i<26;i++){
             mp['A'+i] = -n;
         }
+        
         while(!pq.empty()){
             for(int i=0;i<=n;i++){
                 if(pq.empty()){ time++; continue;}
                 auto top = pq.top();
                 pq.pop();
+                
                 if(mp[top.second]+n<time){
                     mp[top.second] = time;
                     top.first--;
-                    if(top.first!=0) temp.push(top);
+                    
                 }
-                else{
-                    if(top.first!=0) temp.push(top);
-                }
+                
+                if(top.first!=0) temp.push(top);
                 if(temp.empty() && pq.empty()) return time;
                 time++;
             }
