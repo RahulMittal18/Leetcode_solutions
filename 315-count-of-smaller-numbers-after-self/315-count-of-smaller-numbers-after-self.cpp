ae85fb1c84858dd17b2c1fcc8344dfde2e4b8ff0
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-07-23 16:47:31 +0530

    Time: 377 ms (80.02%), Space: 78.9 MB (83.87%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,23 +1,61 @@
-#include <ext/pb_ds/assoc_container.hpp> // Common file
-#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
-#include <ext/pb_ds/detail/standard_policies.hpp>
-using namespace __gnu_pbds;
 
 
-
-typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
 class Solution {
 public:
+    void mergeSort(vector<pair<int,int>> &arr, vector<pair<int,int>> &temp, int left, int right,vector<int>&ans)
+{
+    int mid, inv_count = 0;
+    if (right > left) {
+        mid = (right + left) / 2;
+        mergeSort(arr, temp, left, mid,ans);
+        mergeSort(arr, temp, mid + 1, right,ans);
+  
+        merge(arr, temp, left, mid + 1, right,ans);
+    }
+}
+  
+void merge(vector<pair<int,int>> &arr, vector<pair<int,int>> &temp, int left, int mid, int right,vector<int>&ans)
+{
+    int i, j, k;
+    int count = 0;
+  
+    i = left; /* i is index for left subarray*/
+    j = mid; /* j is index for right subarray*/
+    k = left; /* k is index for resultant merged subarray*/
+    while ((i <= mid - 1) && (j <= right)) {
+        if (arr[i].first <= arr[j].first) {
+            ans[arr[i].second]+=count;
+            temp[k++] = arr[i++];
+        }
+        else {
+            count++;
+            temp[k++] = arr[j++];
+        }
+    }
+  
+    while (i <= mid - 1){
+        ans[arr[i].second]+=count;
+        temp[k++] = arr[i++];
+    }
+  
+    while (j <= right)
+        temp[k++] = arr[j++];
+    for (i = left; i <= right; i++)
+        arr[i] = temp[i];
+  
+}
+
+    
     vector<int> countSmaller(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans(n);
-        ordered_set st;
-        for(int i=n-1;i>=0;i--){
-
-            ans[i]=st.order_of_key(nums[i]);
-            st.insert(nums[i]);
+        vector<pair<int,int>> arr(n),temp(n);
+        for(int i=0;i<n;i++){
+            arr[i].first=nums[i];
+            arr[i].second=i;        
         }
+        vector<int> ans(n,0);
+        mergeSort(arr,temp,0,n-1,ans);
         return ans;
     }
 };
