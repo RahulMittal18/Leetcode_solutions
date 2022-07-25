21a6e59ccf508d0385a45bfacbaca62eeec0fbe9
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-07-25 11:34:17 +0530

    Time: 10 ms (73.39%), Space: 13.6 MB (83.66%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,50 +1,16 @@
 class Solution {
 public:
-   int searchF(vector<int>& nums, int target) {
-       int low = 0;
-       int flag=-1;
-        int high = nums.size()-1;
-       while(low<=high){
-           int mid = low + (high-low)/2;
-           if(nums[mid]==target)
-           {
-               flag=mid;
-               high=mid-1;
-           }
-           else if(nums[mid]>target)
-                high=mid-1;
-           else
-               low=mid+1;
-       }
-       return flag;
-   }
-    
-    int searchL(vector<int>& nums, int target) {
-        int low = 0;
-        int flag=-1;
-        int high = nums.size()-1;
-       while(low<=high){
-           int mid = low + (high-low)/2;
-           if(nums[mid]==target)
-           {
-               flag=mid;
-               low=mid+1;
-           }
-           else if(nums[mid]>target)
-                high=mid-1;
-           else
-               low=mid+1;
-       }
-        return flag;
-   }
-    
-    
+
     vector<int> searchRange(vector<int>& nums, int target) {
-        if(nums.size()==0)
-            return {-1,-1};
-        int a1 = searchF(nums,target);
-        int a2 = searchL(nums,target);
-        return {a1,a2};
+        auto low = lower_bound(nums.begin(),nums.end(),target);
+        auto up = upper_bound(nums.begin(),nums.end(),target);
+        
+        if(!binary_search(nums.begin(),nums.end(),target))    return {-1,-1};
+        
+        int first = low - nums.begin();
+        int last = up - nums.begin()-1;
+        
+        return {first ,last};
         
     }
 };
