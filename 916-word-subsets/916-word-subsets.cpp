ce3da4b42948137054ce869ebf4c0ce4b99d748c
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-07-30 13:14:28 +0530

    Time: 335 ms (47.42%), Space: 102.4 MB (40.67%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
+    vector<int> counter(string a){
+        vector<int> res(26,0);
+        for(int i=0;i<a.size();i++){
+            res[a[i]-'a']++;
+        }
+        return res;
+    }
+    
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
         vector<int> v1(26,0);
-        for(int i=0;i<words2.size();i++){
-            vector<int> temp(26,0);
-            for(int j=0;j<words2[i].size();j++){
-                temp[words2[i][j]-'a']++;
-            }
+        for(auto word : words2){
+            vector<int> temp = counter(word);
             for(int k=0;k<26;k++){
-                if(temp[k]>v1[k]) v1[k]=temp[k];
+                v1[k]=max(v1[k],temp[k]);
             }
         }
         vector<string> ans;
-        for(int i=0;i<words1.size();i++){
-            vector<int> m2(26,0);
-            for(auto c:words1[i]){
-                m2[c-'a']++;
-            }
+        for(auto word: words1){
+            vector<int> m2 = counter(word);
             int k;
             for(k=0;k<26;k++){
                 if(m2[k]<v1[k]){
                     break;
                 } 
             }
-            if(k==26) ans.push_back(words1[i]);
+            if(k==26) ans.push_back(word);
         }
         return ans;
         
