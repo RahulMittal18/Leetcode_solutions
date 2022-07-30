7802ca746c520f2bb2451ccb2093809dc1e45788
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-07-30 22:22:47 +0530

    Time: 121 ms (34.43%), Space: 28 MB (6.77%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     
-    int solve(int i,int j,string &s,string &p, int n,int m,vector<vector<int>> &dp){
+    bool solve(int i,int j,string &s,string &p, int n,int m,vector<vector<int>> &dp){
         if(i==n || j==m){
             if(i==n) {
-            for(int k=j;k<m;k++) if(p[k]!='*') return 0;
-            return 1;
+            for(int k=j;k<m;k++) if(p[k]!='*') return false;
+            return true;
         }
-            return 0;
+            return false;
         }
         if(dp[i][j]!=-1) return dp[i][j];
         int ans = 0;
         if(s[i]==p[j] || p[j]=='?') return dp[i][j] = solve(i+1,j+1,s,p,n,m,dp);
         else if(p[j]=='*'){
-            for(int k=i;k<=n;k++)
-            ans|= solve(k,j+1,s,p,n,m,dp);
+            return dp[i][j]= solve(i,j+1,s,p,n,m,dp)| solve(i+1,j,s,p,n,m,dp) ;
         }
-        else return dp[i][j]=0;
-        return dp[i][j]=ans;
+        else return dp[i][j]=false;
     }
     
     bool isMatch(string s, string p) {
