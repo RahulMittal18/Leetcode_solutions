50f610d4ae51975806867866a31239fe0034a470
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-08-07 13:09:35 +0530

    Time: 223 ms (20.09%), Space: 59.6 MB (12.46%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    unordered_map<char,vector<char>> mp;
-    long long int mod = 1e9+7;
-    long long solve(int i,int n,vector<vector<int>> &dp,char p){
+    long long solve(unordered_map<char,vector<char>> &mp,int i,int n,vector<vector<int>> &dp,char p){
         if(i==n) return 1;
         if(dp[i][p-'a']!=-1) return dp[i][p-'a'];
         
         long long temp=0;
+        int mod = 1e9+7;
         for(auto c:mp[p]){
-            temp=(0ll+temp+solve(i+1,n,dp,c))%mod;
+            temp=(temp+solve(mp,i+1,n,dp,c))%(mod);
         }
         return dp[i][p-'a']=temp;
     }
     
     int countVowelPermutation(int n) {
+        unordered_map<char,vector<char>> mp;
         vector<vector<int>> dp(n,vector<int>(26,-1));
         mp['b']={'a','e','i','o','u'};
         mp['a']={'e'};
@@ -21,6 +21,6 @@ public:
         mp['i']={'a','e','o','u'};
         mp['o']={'i','u'};
         mp['u']={'a'};
-        return solve(0,n,dp,'b');
+        return solve(mp,0,n,dp,'b');
     }
 };
