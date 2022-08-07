c89e0f4f1be467f82ab169d1c3f56a4f0a087708
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-08-07 13:07:14 +0530

    Time: 273 ms (16.20%), Space: 59.5 MB (12.46%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -4,23 +4,23 @@ public:
     long long int mod = 1e9+7;
     long long solve(int i,int n,vector<vector<int>> &dp,char p){
         if(i==n) return 1;
-        if(dp[i][150-p]!=-1) return dp[i][150-p];
+        if(dp[i][p-'a']!=-1) return dp[i][p-'a'];
         
         long long temp=0;
         for(auto c:mp[p]){
             temp=(0ll+temp+solve(i+1,n,dp,c))%mod;
         }
-        return dp[i][150-p]=temp%mod;
+        return dp[i][p-'a']=temp;
     }
     
     int countVowelPermutation(int n) {
-        vector<vector<int>> dp(n,vector<int>(150,-1));
-        mp['A']={'a','e','i','o','u'};
+        vector<vector<int>> dp(n,vector<int>(26,-1));
+        mp['b']={'a','e','i','o','u'};
         mp['a']={'e'};
         mp['e']={'a','i'};
         mp['i']={'a','e','o','u'};
         mp['o']={'i','u'};
         mp['u']={'a'};
-        return solve(0,n,dp,'A');
+        return solve(0,n,dp,'b');
     }
 };
