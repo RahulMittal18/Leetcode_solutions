4140793b71120f516f81a0ccc2431506bd47d95b
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-08-13 12:55:05 +0530

    Time: 505 ms (5.21%), Space: 11.7 MB (10.03%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -3,13 +3,14 @@ public:
     int numTeams(vector<int>& r) {
         int n = r.size();
         int ans=0;
-        vector<vector<int>> dpi(n,vector<int>(4,0));
-        vector<vector<int>> dpd(n,vector<int>(4,0));
-        dpi[0][1]=1;
-        for(int k=1;k<4;k++){
+        vector<vector<int>> dpi(n,vector<int>(3,0));
+        vector<vector<int>> dpd(n,vector<int>(3,0));
+        
+        dpi[0][0]=1;
+        for(int k=0;k<3;k++){
             for(int i=0;i<n;i++){
                 for(int j=0;j<i;j++){
-                    if(k==1) dpi[i][k]=1;
+                    if(k==0) dpi[i][k]=1;
                     else{
                         if(r[i]>r[j]){
                             dpi[i][k] += dpi[j][k-1];
@@ -20,12 +21,11 @@ public:
         }
         
         
-        dpd[0][1]=1;
-
-        for(int k=1;k<4;k++){
+        dpd[0][0]=1;
+        for(int k=0;k<3;k++){
             for(int i=0;i<n;i++){
                 for(int j=0;j<i;j++){
-                    if(k==1) dpd[i][k]=1;
+                    if(k==0) dpd[i][k]=1;
                     else{
                         if(r[i]<r[j]){
                             dpd[i][k] += dpd[j][k-1];
@@ -34,16 +34,8 @@ public:
                 }
             }
         }
-        
-        // for(int i=0;i<n;i++){
-        //     for(int k=1;k<4;k++){
-        //         cout<<dpd[i][k]<<" \n"[k==3];
-        //     }
-        // }
-        
-        
         for(int i=0;i<n;i++){
-            ans+=(dpi[i][3]+dpd[i][3]);
+            ans+=(dpi[i][2]+dpd[i][2]);
         }
         return ans;
     }
