bca2e424e187a9246ca12c7d8d62db9e6378fbb7
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-08-13 12:58:27 +0530

    Time: 480 ms (5.60%), Space: 11.7 MB (11.68%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -7,33 +7,26 @@ public:
         vector<vector<int>> dpd(n,vector<int>(3,0));
         
         dpi[0][0]=1;
-        for(int k=0;k<3;k++){
-            for(int i=0;i<n;i++){
-                for(int j=0;j<i;j++){
-                    if(k==0) dpi[i][k]=1;
-                    else{
-                        if(r[i]>r[j]){
-                            dpi[i][k] += dpi[j][k-1];
-                        }
-                    }
-                }
-            }
-        }
-        
-        
         dpd[0][0]=1;
         for(int k=0;k<3;k++){
             for(int i=0;i<n;i++){
                 for(int j=0;j<i;j++){
-                    if(k==0) dpd[i][k]=1;
+                    if(k==0) {
+                        dpi[i][k]=1;
+                        dpd[i][k]=1;
+                    }
                     else{
-                        if(r[i]<r[j]){
+                        if(r[i]>r[j]){
+                            dpi[i][k] += dpi[j][k-1];
+                        }
+                        else{
                             dpd[i][k] += dpd[j][k-1];
                         }
                     }
                 }
             }
         }
+        
         for(int i=0;i<n;i++){
             ans+=(dpi[i][2]+dpd[i][2]);
         }
