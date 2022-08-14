24be235e937d46a06ded499e0b1a56d8273146c7
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-08-14 15:25:40 +0530

    Time: 85 ms (15.82%), Space: 9.8 MB (63.69%) - LeetHub

---
+++ 126-word-ladder-ii/126-word-ladder-ii.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    
+    
+    void dfs(string &beginWord,string &endWord,vector<string>&path, vector<vector<string>>&ans,unordered_map<string,vector<string>>&adj ){
+        path.push_back(beginWord);
+        if(beginWord==endWord){
+            ans.push_back(path);
+            path.pop_back();
+            return;
+        }
+        for(auto w:adj[beginWord]){
+            dfs(w,endWord,path,ans,adj);
+        }
+        path.pop_back();
+    }
+    
+    
+    
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        set<string> s(wordList.begin(), wordList.end());
+        unordered_map<string,int> level;
+        unordered_map<string,vector<string>> adj;
+        queue<string> q;
+        q.push(beginWord);
+        level[beginWord]=0;
+        while(!q.empty()){
+            int n = q.size();
+            while(n--){
+                auto popped = q.front();
+                q.pop();
+                for(int i=0;i<popped.size();i++){
+                    for(int j=0;j<26;j++){
+                        string temp = popped;
+                        temp[i] = 'a' + j;
+                        if(s.count(temp)==1){
+                            if(level.find(temp)==level.end()){
+                                q.push(temp);
+                                level[temp]=1+level[popped];
+                                adj[temp].push_back(popped);
+                            }
+                            else if(level[temp]==1+level[popped]){
+                                adj[temp].push_back(popped);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        vector<vector<string>> ans;
+        vector<string> path;
+        dfs(endWord,beginWord,path,ans,adj);
+        for(auto &k:ans){
+            reverse(k.begin(),k.end());
+        }
+        return ans;
+    }
+};
+    
