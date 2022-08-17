2d0159c0e960c15960af5fddcc6feb4a186b0ee5
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-08-17 22:49:45 +0530

    Time: 236 ms (74.88%), Space: 51.2 MB (20.94%) - LeetHub

---
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPossibleDivide(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        map<int,int> mp;
+        for(auto num:nums) mp[num]++;
+        int i=0;
+        int n = nums.size();
+        while(i<n){
+            if(mp[nums[i]]>0){
+                for(int j=0;j<k;j++){
+                    if(mp[nums[i]+j]>0){
+                        mp[nums[i]+j]--;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+            i++;
+        }
+        return true;
+    }
+};
