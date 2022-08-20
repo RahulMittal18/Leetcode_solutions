a4bfe9b94a50c5a1047bfaf024641406885007ca
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-08-20 17:45:46 +0530

    Time: 37 ms (73.88%), Space: 17.2 MB (13.81%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -1,21 +1,5 @@
 class Solution {
-public:
-    vector<int> dp;
-
-    int solve(int i,int pos,int target, int currFuel, vector<vector<int>>&stations){
-        if(currFuel<0) return 502;
-        if(i==stations.size()-1){
-            if(currFuel>=target-pos) return 0;
-            else if(currFuel+stations[i][1]>=target-pos) return 1;
-            else return 502;
-        }
-        if(dp[i]!=-1) return dp[i];
-        int not_filled = solve(i+1,stations[i+1][0],target,currFuel-(stations[i+1][0]-stations[i][0]),stations);
-        int filled = 1 + solve(i+1,stations[i+1][0],target,currFuel-(stations[i+1][0]-stations[i][0])+stations[i][1],stations);
-        return dp[i] = min(filled,not_filled);
-    }
-    
-    
+public:    
     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
         int n=stations.size();
         int ans=0,prev=0;
