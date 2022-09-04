500cec4c1dfe83f8d629ffbe2e66daf8562c3c9b
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-09-05 01:56:18 +0530

    Time: 9 ms (49.02%), Space: 14.4 MB (15.94%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,31 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
+    
+    void dfs(TreeNode* root,int i,int j,map<pair<int,int>,vector<int>> &mp){
+        if(!root) return;
+        mp[{j,i}].push_back(root->val);
+        dfs(root->left,i+1,j-1,mp);
+        dfs(root->right,i+1,j+1,mp);
+
+    }
+    
+    
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        queue<pair<TreeNode*,pair<int,int>>> q;
-        q.push({root,{0,0}});
-        map<int,map<int,multiset<int>>> mp;
-        while(!q.empty()){
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                TreeNode* node = q.front().first;
-                int col = q.front().second.first;
-                int lvl = q.front().second.second;
-                mp[col][lvl].insert(node->val);
-                q.pop();
-                if(node->left)
-                    q.push({node->left,{col-1,lvl+1}});
-                if(node->right)
-                    q.push({node->right,{col+1,lvl+1}});
-            }
-        }
+        if(!root) return {};
+        map<pair<int,int>,vector<int>> mp;
+        dfs(root,0,0,mp);
         vector<vector<int>> ans;
-        for(auto x:mp){
-            vector<int>temp;
-            for(auto y:x.second){
-                temp.insert(temp.end(),y.second.begin(),y.second.end());
+        int prev = -10000;
+        vector<int> temp;
+        for(auto k:mp){
+            vector<int> a = k.second;
+            sort(a.begin(),a.end());
+            if(prev==-10000 || k.first.first == prev){
+                prev = k.first.first;
+                temp.insert(temp.end(),a.begin(),a.end());
             }
-            ans.push_back(temp);
+            else{
+                ans.push_back(temp);
+                temp.clear();
+                prev = k.first.first;
+                temp.insert(temp.end(),a.begin(),a.end());
+            }
         }
+        ans.push_back(temp);
         return ans;
     }
 };
