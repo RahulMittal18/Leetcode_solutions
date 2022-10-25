4e1bd0b484d49836717c588ee4e19f9aefb73a3c
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-10-25 20:03:46 +0530

    Time: 182 ms (80.67%), Space: 74.1 MB (74.55%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
@@ -1,15 +1,37 @@
 class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
-        set<int> window; 
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > k) window.erase(nums[i-k-1]);
-            // |x - nums[i]| <= t  ==> -t <= x - nums[i] <= t;
-            auto pos = window.lower_bound(nums[i] - t); // x-nums[i] >= -t ==> x >= nums[i]-t 
-            // x - nums[i] <= t ==> |x - nums[i]| <= t    
-            if (pos != window.end() && *pos - nums[i] <= t) return true;
-            window.insert(nums[i]);
+        int n = nums.size();
+        
+        if(n == 0 || k < 0  || t < 0) return false;
+        
+        unordered_map<int,int> buckets;
+        
+        for(int i=0; i<n; ++i) {
+            int bucket = nums[i] / ((long)t + 1);
+            
+			// For negative numbers, we need to decrement bucket by 1
+			// to ensure floor division.
+			// For example, -1/2 = 0 but -1 should be put in Bucket[-1].
+			// Therefore, decrement by 1.
+            if(nums[i] < 0) --bucket;
+            
+            if(buckets.find(bucket) != buckets.end()) return true;
+            else {
+                buckets[bucket] = nums[i];
+                if(buckets.find(bucket-1) != buckets.end() && (long) nums[i] - buckets[bucket-1] <= t) return true;
+                if(buckets.find(bucket+1) != buckets.end() && (long) buckets[bucket+1] - nums[i] <= t) return true;
+                
+                if(buckets.size() > k) {
+                    int key_to_remove = nums[i-k] / ((long)t + 1);
+                    
+                    if(nums[i-k] < 0) --key_to_remove;
+                    
+                    buckets.erase(key_to_remove);
+                }
+            }
         }
+        
         return false;
     }
 };
