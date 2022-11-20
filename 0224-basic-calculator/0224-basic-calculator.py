f234b244a86cca7a5530e3fa0247a3846411c4a7
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-11-20 23:59:18 +0530

    Time: 206 ms (49.30%), Space: 15.2 MB (75.35%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def calculate(self, s):
+        res, num, sign, stack = 0, 0, 1, []
+        for ss in s:
+            if ss.isdigit():
+                num = 10*num + int(ss)
+            elif ss in ["-", "+"]:
+                res += sign*num
+                num = 0
+                sign = [-1, 1][ss=="+"]
+            elif ss == "(":
+                stack.append(res)
+                stack.append(sign)
+                sign, res = 1, 0
+            elif ss == ")":
+                res += sign*num
+                res *= stack.pop()
+                res += stack.pop()
+                num = 0
+        return res + num*sign
