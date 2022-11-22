7d7a736c45aad38506a4c47c72ccda76bfe57c08
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-11-22 17:44:35 +0530

    Time: 4 ms (98.05%), Space: 6.2 MB (91.16%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numSquares(int n) 
+    {
+        if (n <= 0) return 0;        
+        
+        // cntPerfectSquares[i] = the least number of perfect square numbers 
+        // which sum to i. Since cntPerfectSquares is a static vector, if 
+        // cntPerfectSquares.size() > n, we have already calculated the result 
+        // during previous function calls and we can just return the result now.
+        static vector<int> cntPerfectSquares({0});
+        
+        // While cntPerfectSquares.size() <= n, we need to incrementally 
+        // calculate the next result until we get the result for n.
+        while (cntPerfectSquares.size() <= n)
+        {
+            int m = cntPerfectSquares.size();
+            int cntSquares = INT_MAX;
+            for (int i = 1; i*i <= m; i++)
+            {
+                cntSquares = min(cntSquares, cntPerfectSquares[m - i*i] + 1);
+            }
+            
+            cntPerfectSquares.push_back(cntSquares);
+        }
+        
+        return cntPerfectSquares[n];
+    }
+};
