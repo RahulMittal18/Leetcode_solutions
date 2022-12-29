fa790a80bf742482a23a955e4c8dadf8e77803e4
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2022-12-29 14:50:17 +0530

    Time: 732 ms (78.04%), Space: 150.6 MB (33.60%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-    vector<int> getOrder(vector<vector<int>>& tasksArr) {
-        map<vector<int>,int> mp;
-        int n = tasksArr.size();
+    vector<int> getOrder(vector<vector<int>>& givenTasks) {
+        int n = givenTasks.size();
         vector<int> ans;
         vector<vector<int>> tasks;
-        for(int i=0;i<n;i++) tasks.push_back({tasksArr[i][0],tasksArr[i][1],i});
+        for(int i=0;i<n;i++) tasks.push_back({givenTasks[i][0],givenTasks[i][1],i});
         sort(tasks.begin(),tasks.end());
 
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
@@ -14,13 +13,11 @@ public:
         while(i<n || !pq.empty()){
             while(i<n && t>=tasks[i][0]){
                 pq.push({tasks[i][1],tasks[i][2]});
-                // cout<<t<<"came"<<i<< " "<<pq.top().first<<" "<<pq.top().second<<endl;
                 i++;
             }
             if(i<n && pq.empty()) t=tasks[i][0];
             if(!pq.empty()){
                 t+=pq.top().first;
-                // cout<<t<<"done"<<" "<<pq.top().second<<endl;
                 ans.push_back(pq.top().second);
                 pq.pop();
             }  
