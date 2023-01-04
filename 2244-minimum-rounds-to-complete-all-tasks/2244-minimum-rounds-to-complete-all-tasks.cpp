e2e3c04f0a2d63c56d6887f2f16c6866c3fa2b14
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2023-01-04 12:12:42 +0530

    Time: 235 ms (84.30%), Space: 103.7 MB (45.14%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -4,10 +4,10 @@ public:
         unordered_map<int,int> mp;
         for(int x:tasks) mp[x]++;
         int ans = 0;
-        for(auto k:mp){
-            if(k.second<2) return -1;
-            ans+=k.second/3;
-            if(k.second%3!=0) ans++;
+        for(auto [k,v]:mp){
+            if(v<2) return -1;
+            ans+=v/3;
+            if(v%3!=0) ans++;
         }
         return ans;
     }
