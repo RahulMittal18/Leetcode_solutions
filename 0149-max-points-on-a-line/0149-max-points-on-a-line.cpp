7a79e35d8ac9daedeb619bc4a6bdfd096128f160
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2023-01-08 10:47:05 +0530

    Time: 399 ms (11.61%), Space: 7.4 MB (81.50%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        int ans = 1;
+        unordered_map<int,int> mp;
+        for(int j=0;j<points.size();j++){
+            // for(int i=j+1;i<points.size();i++){
+                // int num = points[i][1]-points[j][1];
+                // int den = points[i][0]-points[j][0];
+                mp[points[j][0]]++;
+            // }
+        }
+        for(int j=0;j<points.size();j++){
+            for(int i=j+1;i<points.size();i++){
+                int num = points[i][1]-points[j][1];
+                int den = points[i][0]-points[j][0];
+                int curr = 2;
+                if(den==0) continue;
+                for(int k=0;k<points.size();k++){
+                    if(i!=k && j!=k){
+                        int n = points[k][1]-points[j][1];
+                        int d = points[k][0]-points[j][0];
+                        if(d*num==n*den) curr++;    
+                    }
+                }
+                ans=max(ans,curr);
+            }
+        }
+        // cout<<ans<<endl;
+        for(auto k:mp){
+            // cout<<k.first<<" "<<k.second<<endl;
+            ans = max(ans,k.second);
+        }
+        return ans;
+    }
+};
