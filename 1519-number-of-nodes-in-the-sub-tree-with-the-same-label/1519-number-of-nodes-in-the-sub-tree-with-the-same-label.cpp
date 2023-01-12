f19974217d696590e68e4b3f02ab986be8fcead0
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2023-01-12 15:30:00 +0530

    Time: 681 ms (74.08%), Space: 230.9 MB (30.37%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     
-    vector<int> solve(vector<vector<int>> &adj,int src,string &labels,int par,vector<int> &freq,vector<int> &ans,int n){
+    vector<int> solve(vector<vector<int>> &adj,int src,string &labels,int par,vector<int> &ans){
         vector<int> temp(26,0);
         for(auto nei:adj[src]){
-            vector<int> child(26,0);
-            if(nei!=par){
-                child = solve(adj,nei,labels,src,freq,ans,n);
-            }
+            if(nei==par) continue;
+            vector<int> child = solve(adj,nei,labels,src,ans);
+            
             for(int i=0;i<26;i++){
                 temp[i]+=child[i];
             }
@@ -18,15 +17,13 @@ public:
     }
     
     vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
-        vector<int> freq(26,0);
         vector<vector<int>> adj(n);
         for(auto e:edges){
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
         }
         vector<int> ans(n,0);
-        // for(auto e:freq) cout<<e<<endl;
-        solve(adj,0,labels,-1,freq,ans,n);
+        solve(adj,0,labels,-1,ans);
         return ans;
     }
 };
