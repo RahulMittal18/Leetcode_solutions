7b4ff0c22f0c81c6a73c977f6006341866e61379
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2023-01-13 19:27:48 +0530

    Time: 407 ms (89.00%), Space: 177.7 MB (78.24%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int dfs(int currentNode, vector<vector<int>>& children, string& s, int& longestPath) {
-        // Longest and second longest chains starting from currentNode (does not count the
-        // currentNode itself).
         int longestChain = 0, secondLongestChain = 0;
         for (int child : children[currentNode]) {
-            // Get the number of nodes in the longest path in the subtree of child,
-            // including the child.
             int longestChainStartingFromChild = dfs(child, children, s, longestPath);
-            // We won't move to the child if it has the same character as the currentNode.
             if (s[currentNode] == s[child]) {
                 continue;
             }
-            // Modify the longestChain and secondLongestChain if longestChainStartingFromChild
-            // is bigger.
             if (longestChainStartingFromChild > longestChain) {
                 secondLongestChain = longestChain;
                 longestChain = longestChainStartingFromChild;
@@ -21,8 +14,6 @@ public:
                 secondLongestChain = longestChainStartingFromChild;
             }
         }
-
-        // Add "1" for the node itself.
         longestPath = max(longestPath, longestChain + secondLongestChain + 1);
         return longestChain + 1;
     }
@@ -30,7 +21,6 @@ public:
     int longestPath(vector<int>& parent, string s) {
         int n = parent.size();
         vector<vector<int>> children(n);
-        // Start from node 1, since root node 0 does not have a parent.
         for (int i = 1; i < n; i++) {
             children[parent[i]].push_back(i);
         }
