e89bdb904ffa1ff51ee29c0abb434494a088d3b5
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2023-01-23 17:31:23 +0530

    Added solution - LeetHub

---
+++ Geeks And The String - GFG/geeks-and-the-string.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    string removePair(string s) {
+        //6825945466
+        stack<char> st;
+        for(auto c:s){
+            if(st.size() && st.top()==c) st.pop();
+            else st.push(c);
+        }
+        if(st.size()==0) return "-1";
+        string ans = "";
+        while(st.size()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s; 
+        cin>>s;
+        
+        Solution obj;
+        string res = obj.removePair(s);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
