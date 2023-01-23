a3c463268a986beadeeabd80bba5f7ef0afb09f3
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2023-01-23 17:56:45 +0530

    Time: 721 ms (13.97%), Space: 13 MB (59.72%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.cpp
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     int triangleNumber(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        int i=0;
         int n = nums.size();
         int ans = 0;
         int x = 0;
-        for(i;i<n;i++){
+        for(int i=0;i<n;i++){
             if(nums[i]==0) continue;
             for(int j=i+1;j<n;j++){
-                auto xx = lower_bound(nums.begin()+j,nums.end(),nums[i]+nums[j]);
+                int two_sum = nums[i]+nums[j];
+                auto xx = lower_bound(nums.begin()+j,nums.end(),two_sum);
                 int x = xx-(nums.begin()+j);
                 if(xx==nums.end()) ans+=(n-j-1);
                 else{
                     int val = *xx;
                     ans+=x;
-                    if(val>=nums[i]+nums[j]) ans--;
+                    if(val>=two_sum) ans--;
                 }
             }
         }
-        return ans<0?0:ans;
+        return ans;
     }
 };
