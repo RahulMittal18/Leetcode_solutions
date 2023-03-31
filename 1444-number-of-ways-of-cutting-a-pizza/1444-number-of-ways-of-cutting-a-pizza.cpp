d24f6780a5ca3b5e8275036ea9bd786089e0a51e
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2023-03-31 22:40:10 +0530

    Time: 20 ms (86.91%), Space: 7.6 MB (94.50%) - LeetHub

---
+++ 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    int pre[51][51];
+    int dp[51][51][11];
+    int mod = 1e9 + 7;
+    int row,col; // 1 1 0 2
+    int applesCount(int t,int b,int l,int r){
+        return pre[b+1][r+1]+pre[t][l]-pre[b+1][l]-pre[t][r+1];
+    }
+    
+    int solve(int t,int b,int l,int r,int k){
+        if(t>=row || l>=col) return 0;
+        if(k==0){
+            // cout<<"remcut"<<" "<<" "<<applesCount(t,b,l,r)<<endl;
+            if(applesCount(t,b,l,r)>0) return 1;
+            else return 0;
+        }
+        if(dp[t][l][k]!=-1) return dp[t][l][k];
+        int ways = 0;
+        
+        for(int i=t;i<b;i++){
+            // cout<<"rowcut"<<" "<<i<<" "<<applesCount(t,i,l,r)<<endl;
+            if(applesCount(t,i,l,r)>0){
+                ways = (ways + solve(i+1,b,l,r,k-1)) % mod;
+            }
+        }
+        
+        for(int i=l;i<r;i++){
+            // cout<<"colcut"<<" "<<i<<" "<<applesCount(t,b,l,i)<<endl;
+            if(applesCount(t,b,l,i)>0){
+                ways = (ways + solve(t,b,i+1,r,k-1)) % mod;
+            }
+        }
+        
+        return dp[t][l][k]=ways;
+    }
+    
+    int ways(vector<string>& pizza, int k) {
+        memset(pre,0,sizeof(pre));
+        memset(dp,-1,sizeof(dp));
+
+        row = pizza.size();
+        col = pizza[0].size();
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(pizza[i][j]=='A') pre[i+1][j+1]=1;
+                pre[i+1][j+1] += pre[i][j+1] + pre[i+1][j] - pre[i][j];
+                // cout<<pre[i+1][j+1]<<" ";
+            }
+            // cout<<endl;
+        }
+        
+        // for(int i=0;i<=row;i++){
+        //     for(int j=0;j<=col;j++){
+        //         cout<<pre[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        // cout<<applesCount(1,1,0,2);
+        return solve(0,row-1,0,col-1,k-1);
+    }
+};
