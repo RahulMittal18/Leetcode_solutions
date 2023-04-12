127bbd6a52b78195645ed372aea87782592a4b64
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2023-04-12 14:52:22 +0530

    Time: 4 ms (78.99%), Space: 9.6 MB (61.69%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,45 +1,15 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        stack<string> st;
-        int n = path.size();
-        string dir = "";
-        for(int i=0;i<=n;i++){
-            if(i<n && path[i]!='/'){
-                dir.push_back(path[i]);
-            }
-            else{
-                int s = dir.size();
-                if(s==1){
-                    if(dir!=".") st.push(dir);
-                }
-                else if(s==2){
-                    if(dir==".."){
-                        if(st.size()) st.pop();
-                    }
-                    else{
-                        st.push(dir);
-                    }
-                }
-                else if(s>2){
-                    st.push(dir);
-                }
-                dir="";
-            }
+        string res, tmp;
+        vector<string> stk;
+        stringstream ss(path);
+        while(getline(ss,tmp,'/')) {
+            if (tmp == "" or tmp == ".") continue;
+            if (tmp == ".." and !stk.empty()) stk.pop_back();
+            else if (tmp != "..") stk.push_back(tmp);
         }
-        string ans="";
-        stack<string> st2;
-        while(st.size()){
-            st2.push(st.top());
-            st.pop();
-        }
-        while(st2.size()){
-            ans.push_back('/');
-            for(char c:st2.top()){
-                ans.push_back(c);
-            }
-            st2.pop();
-        }
-        return ans.size()>0?ans:"/";
+        for(auto str : stk) res += "/"+str;
+        return res.empty() ? "/" : res;
     }
 };
