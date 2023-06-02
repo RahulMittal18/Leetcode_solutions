1215366a1074f08e950b54e06986fc8ba6962c8e
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2023-06-02 15:31:16 +0530

    Time: 815 ms (60.49%), Space: 16.9 MB (30.34%) - LeetHub

---
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maximumDetonation(self, bombs: List[List[int]]) -> int:
+        graph = collections.defaultdict(list)
+        n = len(bombs)
+        
+        # Build the graph
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    continue         
+                xi, yi, ri = bombs[i]
+                xj, yj, _ = bombs[j]
+
+                # Create a path from node i to node j, if bomb i detonates bomb j.
+                if ri ** 2 >= (xi - xj) ** 2 + (yi - yj) ** 2:
+                    graph[i].append(j)
+
+        # DFS to get the number of nodes reachable from a given node cur
+        def dfs(cur, visited):
+            visited.add(cur)
+            for neib in graph[cur]:
+                if neib not in visited:
+                    dfs(neib, visited)
+            return len(visited)
+        
+        answer = 0
+        for i in range(n):
+            visited = set()
+            answer = max(answer, dfs(i, visited))
+        
+        return answer
