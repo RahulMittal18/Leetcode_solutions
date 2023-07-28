4a2e794d5694c0f20f826007f481d7f5467bdc08
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2023-07-28 20:53:52 +0530

    Time: 35 ms (5.15%), Space: 12.9 MB (5.59%) - LeetHub

---
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool check(int x,vector<vector<int>>& grid){
+        int n = grid.size();
+        int m = grid[0].size();
+        if(grid[0][0]+x<=0) return false;
+        vector<vector<int>> dp(n,vector<int>(m,0));
+        dp[0][0] = grid[0][0]+x;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0) continue;
+                dp[i][j] = -1e7;
+                if(i>0){
+                    dp[i][j] = max(dp[i][j],dp[i-1][j]+grid[i][j]);
+                }
+                if(j>0){
+                    dp[i][j] = max(dp[i][j],dp[i][j-1]+grid[i][j]);
+                }
+                if(dp[i][j]<=0) dp[i][j] = -1e7;
+
+            }
+        }
+        return dp[n-1][m-1]>0;
+        
+    }
+    
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        
+        int l =1;
+        int r =1e7;
+        int ans = INT_MAX;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            // cout<<mid<<endl;
+            if(check(mid,dungeon)){
+                ans = mid;
+                r = mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return ans;
+        
+        // return solve(0,0,dungeon,dp);
+    }
+};
