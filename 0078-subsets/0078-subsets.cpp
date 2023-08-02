c66f6215640576b3557b9d758fd9a3a128548573
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2023-08-02 23:07:25 +0530

    Time: 0 ms (100.00%), Space: 16.7 MB (18.01%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -9,7 +9,7 @@ public:
         return ans;
     }
     
-    void solve(vector<int> nums, int curr, vector<int> &curr_vec, vector<vector<int>> &ans){
+    void solve(vector<int> nums, int curr, vector<int> curr_vec, vector<vector<int>> &ans){
         if(curr == n){                          //Each time when u go out of the array, push the curr array into the result 
             ans.push_back(curr_vec);
             return;
@@ -19,6 +19,6 @@ public:
         curr_vec.push_back(nums[curr]);        //include the current element
         solve(nums, curr+1, curr_vec, ans);   //traverse to the next element
 		
-        curr_vec.pop_back();                 //Backtracing
+        // curr_vec.pop_back();                 //Backtracing
     }
 };
