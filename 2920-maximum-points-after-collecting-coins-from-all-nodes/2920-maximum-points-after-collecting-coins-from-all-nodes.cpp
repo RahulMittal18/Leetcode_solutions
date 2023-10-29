4833fa9113e6ec4707c485738d5c5985e4c12534
Rahul <77724943+RahulMittal18@users.noreply.github.com>
2023-10-29 09:44:04 +0530

    Time: 1677 ms (22.22%), Space: 203.9 MB (77.78%) - LeetHub

---
+++ 2920-maximum-points-after-collecting-coins-from-all-nodes/2920-maximum-points-after-collecting-coins-from-all-nodes.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> adj[100005];
+    int dp[100005][15];
+    int k;
+
+    int dfs2(int src,int par,int times,vector<int> &coins){
+        int choice1 = 0;
+        int choice2 = 0;
+        if(dp[src][times]!=-1) return dp[src][times];
+        for(auto nei:adj[src]){
+            if(par==nei) continue;
+            choice1 += dfs2(nei,src,min(14,times+1),coins);
+            choice2 += dfs2(nei,src,times,coins);
+        }
+        int coinsAvailableAtSource = coins[src]/(1<<times);
+        choice1 += coinsAvailableAtSource/2;
+        choice2 += coinsAvailableAtSource-k;
+
+        return dp[src][times] = max(choice1, choice2);
+    }
+    
+    
+    int maximumPoints(vector<vector<int>>& edges, vector<int>& coins, int kk) {
+        k = kk;
+        // memset(dp,-1,sizeof(dp));
+        for(int i=0;i<100005;i++){
+            for(int j=0;j<15;j++){
+                dp[i][j]=-1;
+            }
+        }
+        for(auto e:edges){
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+        return dfs2(0,-1,0,coins);   
+    }
+};
